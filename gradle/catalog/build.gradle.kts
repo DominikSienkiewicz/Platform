ab840af0be6aff638a46c1c5143c00c8247b2669
Dominik Sienkiewicz <github.shelf261@passmail.net>
2026-06-07 23:21:54 +0200

    Upgrade to latest lib versions

--- gradle/catalog/build.gradle.kts
+++ gradle/catalog/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 }
 
 group = "pl.seniordeveloper"
-version = providers.gradleProperty("platformVersion").getOrElse("1.1.0")
+version = providers.gradleProperty("platformVersion").getOrElse("1.3.1")
 
 catalog {
 	versionCatalog {
 		// ============================ WERSJE ============================
-		version("platform", providers.gradleProperty("platformVersion").getOrElse("1.1.0"))
+		version("platform", providers.gradleProperty("platformVersion").getOrElse("1.3.1"))
 		version("java", "25")
 		version("springBoot", "4.0.6")
 		version("springDependencyManagement", "1.1.7")
