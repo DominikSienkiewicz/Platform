2cec623e0208df18e91a7fced84b148d132a898a
Dominik Sienkiewicz <github.shelf261@passmail.net>
2026-06-08 18:47:57 +0200

    fix: Release bugfix

--- gradle/catalog/build.gradle.kts
+++ gradle/catalog/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 }
 
 group = "pl.seniordeveloper"
-version = providers.gradleProperty("platformVersion").getOrElse("1.3.4")
+version = providers.gradleProperty("platformVersion").getOrElse("1.3.5")
 
 catalog {
 	versionCatalog {
 		// ============================ WERSJE ============================
-		version("platform", providers.gradleProperty("platformVersion").getOrElse("1.3.4"))
+		version("platform", providers.gradleProperty("platformVersion").getOrElse("1.3.5"))
 		version("java", "25")
 		version("springBoot", "4.0.6")
 		version("springDependencyManagement", "1.1.7")
